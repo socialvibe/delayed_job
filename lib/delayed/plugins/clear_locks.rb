cd69f533fcd955e934d6e33495db343dbbc1ebd7
Kenneth Hwang <kenneth@truex.com>
2016-10-27 16:04:06 -0700

    also clear locks on startup in addition to exit, in case there are are stale locks from dead processes

--- lib/delayed/plugins/clear_locks.rb
+++ lib/delayed/plugins/clear_locks.rb
@@ -3,6 +3,7 @@ module Delayed
     class ClearLocks < Plugin
       callbacks do |lifecycle|
         lifecycle.around(:execute) do |worker, &block|
+          Delayed::Job.clear_locks!(worker.name)
           begin
             block.call(worker)
           ensure
